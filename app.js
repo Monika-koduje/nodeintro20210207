5d738bbbdb2888775c6213143f420c6154b1d0d6
MarcinDl <marcindl@hotmail.com>
2021-02-07 13:08:41 +0100

    hbs

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express");
 const port = 3000;
 
 const app = express();
-
+app.set("view engine","hbs")
 app.get("/", function (req,res){
-    res.send("Hello World");
+    res.render("index");
 })
 app.get("/about", function (req,res){
     res.send("Strona o nas");
 })
 
-app.listen(port,()=>{
+app.listen(port,(err)=>{
+    if (err) { return console.log(err)}
     console.log(`Apka działa na porcie ${port}`)
 })
 
