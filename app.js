e2977804d780b5dba29a225b6b84d807b2565152
MarcinDl <marcindl@hotmail.com>
2021-02-07 12:50:25 +0100

    push

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const http = require("http");
 
 const handler = (request, response) => {
+    response.end("<h1>Witajcie na nauce NodeJS<h1>")
     console.log("Ktoś wszedł na stronę")
 }
 
 const server = http.createServer(handler);
 const port = 3000;
 
-server.listen(port, () => {
+server.listen(port, (err) => {
+    if (err) { return console.log(err)}
     console.log(`serwer działa na porcie: ${port}`)
 })
 
